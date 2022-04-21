c1d94fdc307e70045b2d6570e7773987ed935ef8
Tarcio Sillva <tarciosillva7@gmail.com>
2022-04-21 19:08:31 -0300

    Welcome

--- locacaoEstacionamento/App.js
+++ locacaoEstacionamento/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Welcome from './telas/Welcome';
 import Home from './telas/Home';
 import Login from './telas/Login';
@@ -12,8 +12,8 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Tela Inicial" component={Welcome} />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Home" component={Home} />
--- locacaoEstacionamento/telas/Welcome.js
+++ locacaoEstacionamento/telas/Welcome.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ImageBackground, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const image = require('../assets/imageBackgroundWelcome.png')
+
 
 export default function Welcome({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <Button
-        title="Sou novo"
-        onPress={() => navigation.navigate('Cadastro')}
-      />
-      <Button
-        title="Já tenho cadastro"
-        onPress={() => navigation.navigate('Login')}
-      />
-      <StatusBar style="auto" />
+       <ImageBackground source={image} resizeMode='cover' style={styles.image}>
+
+         <View style={styles.contentCenter}>
+          <Text style={styles.welcomeTitle}>Bem vindo</Text>
+          <Text style={styles.textWelcome}>
+            Bem-vindo ao Design Diário, 
+            o maior portal para se informar sobre design
+          </Text>   
+
+          <Pressable style={styles.buttonNewUser} onPress={() => navigation.navigate('Cadastro')}>
+            <Text  style={styles.textButtonNewUser}>Sou novo</Text>
+          </Pressable>
+          <Pressable style={styles.buttonUserRegistred} onPress={() => navigation.navigate('Login')}>
+            <Text  style={styles.textButtonUserRegistred}>Já tenho conta</Text>
+          </Pressable>
+         </View>
+      
+        
+        <StatusBar style="auto" />
+       </ImageBackground>
     </View>
   );
 }
@@ -26,5 +39,56 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0
   },
+  image:{
+    flex: 1,
+    justifyContent:'center',
+  },
+  contentCenter:{
+    backgroundColor:'#fff',
+    padding: 10,
+    margin: 50,
+    borderRadius: 12
+  },
+  welcomeTitle:{
+    textAlign:'center',
+    fontSize: 27,
+    marginBottom:10,
+    fontWeight:'bold'
+  },
+  textWelcome:{
+    textAlign:'center',
+    fontSize:19,
+    padding: 15
+  },
+  buttonNewUser: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    borderRadius: 12,
+    margin: 15,
+    marginBottom:10,
+    backgroundColor: '#5F93FB',
+  },
+  textButtonNewUser:{
+    color: '#fff',
+    fontWeight:'700',
+    fontSize:18,
+    fontFamily:'Quicksand'
+  },
+  buttonUserRegistred: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 15,
+    marginTop:0,
+    backgroundColor: '#fff',
+  },
+  textButtonUserRegistred:{
+    color: '#5F93FB',
+    fontWeight:'700',
+    fontSize:18,
+    fontFamily:'Quicksand'
+  }
 });
