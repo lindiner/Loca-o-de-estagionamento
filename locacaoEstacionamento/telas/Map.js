01aa1041c2f990db4be5832cce6818d7dac2a740
JoathanBR <joathanlopes@hotmail.com>
2022-06-03 23:03:27 -0300

    Adicionando a funcionalidade do google maps na tela de maps

--- locacaoEstacionamento/telas/Map.js
+++ locacaoEstacionamento/telas/Map.js
@@ -1,25 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, ImageBackground, View } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Feather from 'react-native-vector-icons/Feather';
+import { Button, StyleSheet, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 const image = require('../assets/maps.jpg');
 
 export default function Map({ navigation }) {
   return (
     <View style={styles.container}>
-      <ImageBackground source={image} resizeMode='cover' style={styles.image}>
-        <Text style={styles.texto}></Text>
-      </ImageBackground>
-
-      
-      <StatusBar style="auto" />
-        <View style={styles.footer}>
-          <Feather name="home" size={30} marginLeft={25} color={"#B0B0B0"} onPress={() => navigation.navigate('Home')} ></Feather>
-          <Ionicons name="map-outline" size={30} marginLeft={25} color={"#B0B0B0"} onPress={() => navigation.navigate('Map')} ></Ionicons>
-          <Ionicons name="newspaper-outline" size={30} marginLeft={25} color={"#B0B0B0"} onPress={() => navigation.navigate('Record')} ></Ionicons>
-          <Ionicons name="person-outline" size={30} marginLeft={25} color={"#B0B0B0"} onPress={() => navigation.navigate('Profile')} ></Ionicons>
-        </View>
+      <MapView  style={styles.map}
+      initialRegion={{
+        latitude: -5.334651,
+        longitude: -49.088738,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}/>
     </View>
   );
 }
@@ -31,26 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image:{
-    flex: 1,
-    justifyContent:'center',
-  },
-  texto:{
-    padding:20,
-    paddingLeft: 200,
-    paddingRight: 200,
-  },
-  footer:{
-    display: 'flex',
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 0,
-    left: 0, 
-    right: 0,
-    padding:20,
-    paddingLeft: 30,
-    paddingRight: 30,
-    justifyContent: 'space-between',
-    backgroundColor: '#ffffff',
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
